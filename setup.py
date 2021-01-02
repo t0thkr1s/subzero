65ff3c4d412390dd09f5dfb4acdec0ee0b89c8ef
Mr. Robot <t0thkr1s@icloud.com>
2021-01-02 16:59:47 +0100

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='subzero',
+    version='1.0',
+    url='https://medium.com/infosec-adventures',
+    author='https://github.com/t0thkr1s',
+    author_email='t0thkr1s@icoud.com',
+    description='Passive subdomain enumeration tool for bug-bounty hunters & penetration testers.',
+    install_requires=['rich']
+)
